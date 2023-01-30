36f356955a12e00b12c4738eb91d28593074b371
Casper da Costa-Luis <casper.dcl@physics.org>
2023-01-30 14:13:46 +0000

    tests: test SPM12 installation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,14 +3,17 @@ from pytest import importorskip
 
 def test_matlab():
     engine = importorskip("matlab.engine")
-    from spm12.utils import get_matlab
+    from spm12 import utils
 
     assert not engine.find_matlab()
-    eng = get_matlab()
+    eng = utils.get_matlab()
 
     matrix = eng.eval("eye(3)")
     assert matrix.size == (3, 3)
 
     assert engine.find_matlab()
-    eng2 = get_matlab()
+    eng2 = utils.get_matlab()
     assert eng == eng2
+
+    utils.ensure_spm()
+    assert utils.spm_dir_eng() == utils.spm_dir()
